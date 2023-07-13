36f4049d6766d2c7871286b065e9226127dc2f4f
chamikaNylas <79298790+chamikaNylas@users.noreply.github.com>
2023-07-13 14:57:10 +0530

    Add files via upload
    init_1

---
+++ ConsoleApp1/AESEncryption.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class AESEncryption
+    {
+        public static string EncryptQueryParam(string value, string key, string iv)
+        {
+
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(key);
+                aesAlg.IV = Convert.FromBase64String(iv);
+
+                var encryptor = aesAlg.CreateEncryptor();
+                byte[] encryptedBytes;
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(value);
+                        }
+                        encryptedBytes = msEncrypt.ToArray();
+                    }
+                }
+
+                string encryptedValue = Convert.ToBase64String(encryptedBytes);
+                return encryptedValue;
+            }
+        }
+
+        public static string Decrypt(string encryptedData, string key, string iv)
+        {
+            using (var aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(key);
+                aesAlg.IV = Convert.FromBase64String(iv);
+
+                var decryptor = aesAlg.CreateDecryptor();
+                var encryptedBytes = Convert.FromBase64String(encryptedData);
+
+                string decryptedData;
+
+                using (var msDecrypt = new MemoryStream(encryptedBytes))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            decryptedData = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+                return decryptedData;
+            }
+        }
+
+        public static string GenerateKey()
+        {
+            using (Aes aesAlgorithm = Aes.Create())
+            {
+                aesAlgorithm.KeySize = 256;
+                aesAlgorithm.GenerateKey();
+                return Convert.ToBase64String(aesAlgorithm.Key);
+            }
+        }
+        public static string GenerateIV()
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.GenerateIV();
+                return Convert.ToBase64String(aes.IV);
+            }
+        }
+    }
+
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+string key = AESEncryption.GenerateKey();
+string iv = AESEncryption.GenerateIV();
+
+Console.WriteLine("Generated Key:");
+Console.WriteLine(key);
+Console.WriteLine("Generated IV:");
+Console.WriteLine(iv);
+var today=DateTime.Now;
+string value = $"asif.log@gmail.com,898971,{today.ToString()}";
+string encryptedValue = AESEncryption.EncryptQueryParam(value, key, iv);
+Console.WriteLine("Encrypted Value:");
+Console.WriteLine(encryptedValue);
+
+string decryptedValue = AESEncryption.Decrypt(encryptedValue, key, iv);
+Console.WriteLine("Decrypted Value:");
+Console.WriteLine(decryptedValue);
+//using System;
+//using System.IO;
+//using System.Security.Cryptography;
+
+//namespace AesEncryption
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Welcome to the Aes Encryption Test tool");
+//            Console.WriteLine("Provide the Aes Key in base64 format :");
+//            string keyBase64 = Console.ReadLine();
+//            Console.WriteLine("--------------------------------------------------------------");
+//            Console.WriteLine("Please enter the text that you want to encrypt:");
+//            string plainText = Console.ReadLine();
+//            Console.WriteLine("--------------------------------------------------------------");
+//            string cipherText = EncryptDataWithAes(plainText, keyBase64, out string vectorBase64);
+
+//            Console.WriteLine("--------------------------------------------------------------");
+//            Console.WriteLine("Here is the cipher text:");
+//            Console.WriteLine(cipherText);
+
+//            Console.WriteLine("--------------------------------------------------------------");
+//            Console.WriteLine("Here is the Aes IV in Base64:");
+//            Console.WriteLine(vectorBase64);
+//        }
+
+//        private static string EncryptDataWithAes(string plainText, string keyBase64, out string vectorBase64)
+//        {
+//            using (Aes aesAlgorithm = Aes.Create())
+//            {
+//                aesAlgorithm.Key = Convert.FromBase64String(keyBase64);
+//                aesAlgorithm.GenerateIV();
+//                Console.WriteLine($"Aes Cipher Mode : {aesAlgorithm.Mode}");
+//                Console.WriteLine($"Aes Padding Mode: {aesAlgorithm.Padding}");
+//                Console.WriteLine($"Aes Key Size : {aesAlgorithm.KeySize}");
+
+//                //set the parameters with out keyword
+//                vectorBase64 = Convert.ToBase64String(aesAlgorithm.IV);
+
+//                // Create encryptor object
+//                ICryptoTransform encryptor = aesAlgorithm.CreateEncryptor();
+
+//                byte[] encryptedData;
+
+//                //Encryption will be done in a memory stream through a CryptoStream object
+//                using (MemoryStream ms = new MemoryStream())
+//                {
+//                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+//                    {
+//                        using (StreamWriter sw = new StreamWriter(cs))
+//                        {
+//                            sw.Write(plainText);
+//                        }
+//                        encryptedData = ms.ToArray();
+//                    }
+//                }
+
+//                return Convert.ToBase64String(encryptedData);
+//            }
+//        }
+//    }
+//}
+
